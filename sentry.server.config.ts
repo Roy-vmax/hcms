36d8a07a360e378a79be2d7942596dec4a997930
Roy-vmax <129337949+Roy-vmax@users.noreply.github.com>
2025-04-13 15:48:52 +0300

    hcms
    apointtment app

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f79755f3873c8c30324ae66b3c1dc0cc@o4509142327033856.ingest.de.sentry.io/4509142364192848",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
